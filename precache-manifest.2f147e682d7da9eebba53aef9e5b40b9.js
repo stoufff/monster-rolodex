18239f8a1ec7a73dbc705612700dd9196cbd5df4
stoufff <christophe.vandendaele08@gmail.com>
2019-11-24 20:40:36 +0100

    Updates

---
+++ precache-manifest.2f147e682d7da9eebba53aef9e5b40b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47fffb2ef674b33911ae75649105ad0f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "0bac673e3c235efd0ab7",
+    "url": "/monsters-rolodex/static/css/main.f2e17a94.chunk.css"
+  },
+  {
+    "revision": "072144937a437c299fb0",
+    "url": "/monsters-rolodex/static/js/2.12dc0d94.chunk.js"
+  },
+  {
+    "revision": "0bac673e3c235efd0ab7",
+    "url": "/monsters-rolodex/static/js/main.f6c85726.chunk.js"
+  },
+  {
+    "revision": "ec765bb5ff3b7357d49a",
+    "url": "/monsters-rolodex/static/js/runtime-main.db675a29.js"
+  }
+]);
